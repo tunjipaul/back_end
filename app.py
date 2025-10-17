756fc2e87cdbe8d77751f576997cb828db166ec4
TunjiPaul <ogorpaul877@gmail.com>
2025-10-17 15:38:03 +0100

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 import os
 import uvicorn
@@ -8,10 +8,30 @@ load_dotenv()
 
 app = FastAPI(title = "My FastAPI Application", version = "1.0.0")
 
-@app.get("/")
-def root():
-    return {"message": "Welcome to my FastAPI application!"}
+data = [
+    {"name": "Sam Larry", "age": 20, "track": "AI Developer"},
+    {"name": "Bahubili", "age": 21,"track": "Data Scientist"},
+    {"name": "Walter White","age": 22, "track": "Backend Developer"},
+]
 
+class Item(BaseModel):
+    name: str = Field(..., example= "Perpetual")
+    age: int = Field(..., example = 25)
+    track: str = Field(..., example = "Full Stack Developer")
+
+@app.get("/", description="This endpoint just returns a welcome message")
+def root():
+    return {"message": "Welcome to My FastAPI Application!"}
+
+@app.get("/get-data")
+def get_data():
+    return data
+
+@app.post("/create-data")
+def create_data(req: Item):
+    data.append(req.dict())
+    print(data)
+    return {"message": "Data created successfully", "data": data}
 
 if __name__ == "__main__":
     print(os.getenv("host"))

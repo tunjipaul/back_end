8f86a967ae166dbe21cb9a936adefa58ac7b3f14
TunjiPaul <ogorpaul877@gmail.com>
2025-10-21 16:51:55 +0100

    updated

--- database.py
+++ database.py
@@ -1,13 +1,57 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-db_url = f'mysql+pymysql://{os.getenv("dbuser")}:{os.getenv("dbpass")}@{os.getenv("dbhost")}:{os.getenv("dbport")}/{os.getenv("dbname")}'
+# db_url_format = dialect+driver://dbuser;dbpasswd;dbhost;dbport;dbname
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+db_url = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(f"db_url : {db_url}")
+
 engine = create_engine(db_url)
+Session = sessionmaker(bind=engine)
+db = Session()
 
-session = sessionmaker(bind=engine)
+create_table_query = text('''
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+    ); ''')
 
-db = session()
+create_courses_query = text('''
+    CREATE TABLE IF NOT EXISTS courses  (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    level VARCHAR(255) NOT NULL
+    ); ''')
+
+create_enrollment_query = text('''
+    CREATE TABLE IF NOT EXISTS enrollments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL,
+    courseId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users (id),
+    FOREIGN KEY (courseId) REFERENCES courses (id)
+    ); ''')
+
+try:
+    db.execute(create_table_query)
+    db.execute(create_courses_query)
+    db.execute(create_enrollment_query)
+
+    db.commit()
+    print("All Tables created successfully")
+except Exception as e:
+    db.rollback()
+    print(f"Something went wrong: {e}")
+
+finally:
+    db.close()
---
+++ users.py
@@ -0,0 +1,64 @@
+from database import db
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy import text
+import os
+from dotenv import load_dotenv
+import bcrypt
+import uvicorn
+
+load_dotenv()
+
+app = FastAPI(title="Simple App", version="1.0.0")
+
+
+class Simple(BaseModel):
+    name: str = Field(..., example="Sam Larry")
+    email: str = Field(..., example="sam@email.com")
+    password: str = Field(..., example="sam123")
+
+
+@app.post("/signup")
+def signUp(input: Simple):
+    try:
+
+        duplicate_query = text(
+            """
+            SELECT * FROM users
+            WHERE email = :email
+                             """
+        )
+        existing = db.execute(duplicate_query, {"email": input.email})
+        if existing:
+                raise HTTPException(status_code=400, detail="Email already exists")
+
+        query = text(
+            """
+            INSERT INTO users (name, email, password)
+            VALUES (:name, :email, :password)
+        """
+        )
+
+        salt = bcrypt.gensalt()
+        hashedPassword = bcrypt.hashpw(input.password.encode("utf-8"), salt)
+
+        db.execute(
+            query,
+            {"name": input.name, "email": input.email, "password": hashedPassword},
+        )
+        db.commit()
+
+        return {
+            "message": "User created successfully",
+            "data": {"name": input.name, "email": input.email},
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    finally:
+         db.close()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
